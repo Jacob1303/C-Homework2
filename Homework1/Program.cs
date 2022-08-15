7759e1b0b87709b86ba60a7823ba8dc802c02e8d
yakov kuzin <kuzin_js@mail.ru>
2022-08-15 14:48:00 +0300

    Homework1 add.

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] numb)
+    {
+        Console.WriteLine("Введите целое трёхзначное число");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int number1 = Math.Abs(number);
+        if (number1 > 99 && number1 < 1000)
+        {
+            number1 = number1 / 10;
+            number1 = number1 % 10;
+            Console.WriteLine("Вторая цифра введённого Вами числа это " + number1);
+        }
+        else
+        {
+            Console.WriteLine("Нужно ввести корректное число");
+        }
+    }
+}
